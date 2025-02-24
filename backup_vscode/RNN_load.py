f62b8b8391d9406bde9051645631b9af835a9ef7
daraavishan <55817459+daraavishan@users.noreply.github.com>
2025-02-24 18:49:23 -0500

    new rnn models
    new rnn models

--- backup_vscode/RNN_load.py
+++ backup_vscode/RNN_load.py
@@ -3,6 +3,126 @@ from RNN_stuff import load_scaler
 
 
 ######################
+# RETURNS
+######################
+
+model_trailing_return_20_zscore_ema_15min_t1 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/trailing_return_20_zscore_ema_15min_t1.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_trailing_return_20_zscore_ema_15min_t1 = None
+
+model_trailing_return_20_zscore_ema_15min_t2 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/trailing_return_20_zscore_ema_15min_t2.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_trailing_return_20_zscore_ema_15min_t2 = None
+
+model_trailing_return_20_zscore_ema_15min_t3 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/trailing_return_20_zscore_ema_15min_t3.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_trailing_return_20_zscore_ema_15min_t3 = None
+
+model_trailing_return_20_zscore_ema_15min_t4 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/trailing_return_20_zscore_ema_15min_t4.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_trailing_return_20_zscore_ema_15min_t4 = None
+
+model_trailing_return_20_zscore_ema_15min_t5 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/trailing_return_20_zscore_ema_15min_t5.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_trailing_return_20_zscore_ema_15min_t5 = None
+
+
+######################
+# PRICE PERCENTILES 
+######################
+
+model_close_raw_percentile_20_15min_t1 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/close_raw_percentile_20_15min_t1.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_close_raw_percentile_20_15min_t1 = None
+
+model_close_raw_percentile_20_15min_t2 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/close_raw_percentile_20_15min_t2.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_close_raw_percentile_20_15min_t2 = None
+
+model_close_raw_percentile_50_15min_t1 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/close_raw_percentile_50_15min_t1.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_close_raw_percentile_50_15min_t1 = None
+
+model_close_raw_percentile_50_15min_t2 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/close_raw_percentile_50_15min_t2.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_close_raw_percentile_50_15min_t2 = None
+
+model_close_raw_percentile_50_15min_t3 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/close_raw_percentile_50_15min_t3.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_close_raw_percentile_50_15min_t3 = None
+
+
+######################
+# LOAD RNN MODELS (vwap)
+######################
+
+
+# Model t6
+model_vwap_zscore_10_15min_t6 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/vwap_zscore_10_15min_t6.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_vwap_zscore_10_15min_t6 = None
+
+# Model t7
+model_vwap_zscore_10_15min_t7 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/vwap_zscore_10_15min_t7.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_vwap_zscore_10_15min_t7 = None
+
+# Model t8
+model_vwap_zscore_10_15min_t8 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/vwap_zscore_10_15min_t8.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_vwap_zscore_10_15min_t8 = None
+
+# Model t9
+model_vwap_zscore_10_15min_t9 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/vwap_zscore_10_15min_t9.pth',
+    input_size=7,
+    num_features=8
+)
+scaler_vwap_zscore_10_15min_t9 = None
+
+
+
+
+######################
 # LOAD RNN MODELS (new)
 ######################
 
@@ -37,6 +157,39 @@ model_sqz_momentum_slope_30_15min_t4 = load_model(
     num_features=6
 )
 scaler_sqz_momentum_30_15min_t4 = None
+
+# sqz_momentum_slope_30
+model_sqz_momentum_slope_30_15min_t5 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/sqz_momentum_slope_30_15min_t5.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_sqz_momentum_30_15min_t5 = None
+
+# sqz_momentum_slope_30
+model_sqz_momentum_slope_30_15min_t6 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/sqz_momentum_slope_30_15min_t6.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_sqz_momentum_30_15min_t6 = None
+
+# sqz_momentum_slope_30
+model_sqz_momentum_slope_30_15min_t7 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/sqz_momentum_slope_30_15min_t7.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_sqz_momentum_30_15min_t7 = None
+
+# sqz_momentum_slope_30
+model_sqz_momentum_slope_30_15min_t8 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/sqz_momentum_slope_30_15min_t8.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_sqz_momentum_30_15min_t8 = None
+
 
 
 
@@ -73,6 +226,14 @@ model_sqz_momentum_30_15min_t4 = load_model(
     num_features=6
 )
 scaler_sqz_momentum_30_15min_t4 = None
+
+# sqz_momentum_slope_30
+model_sqz_momentum_30_15min_t5 = load_model(
+    model_path='C:/Users/daraa/Desktop/vscode_python/RNN_models/sqz_momentum_30_15min_t5.pth',
+    input_size=7,
+    num_features=6
+)
+scaler_sqz_momentum_30_15min_t5 = None
 
 
 
